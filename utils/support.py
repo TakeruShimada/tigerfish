b2bebe100780ae74ee69c56dc23d5e40c7878590
Hirotaka Kawashima <k.hirotaka@me.com>
2020-11-20 21:50:56 +0900

    #7 easy & hard モードの追加

--- utils/support.py
+++ utils/support.py
@@ -3,13 +3,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
 
-def fetch_titanic(train=True):
+def fetch_titanic(mode="easy", train=True):
+    """タイタニックデータセットの読み込みを行う関数
+
+    Example:
+        >>> (x_train, y_train), x_test = fetch_titanic(mode="easy")
+        >>> # or
+        >>> (x_train, y_train), x_test = fetch_titanic(mode="hard")
+
+    Args:
+        mode: titanicデータセットの難易度設定 (default: easy)
+        train: if True, return (x_train, y_train), x_test
+    """
+    if mode == "easy":
+        test_size = 0.3
+    else:
+        test_size = 0.98
+
     original = sns.load_dataset("titanic")
     data = original.drop(columns="survived")
     targets = original["survived"]
 
     x_train, x_test, y_train, y_test = train_test_split(
-        data, targets, test_size=0.3, random_state=0, shuffle=True
+        data, targets, test_size=test_size, random_state=0, shuffle=True
     )
 
     if train:
